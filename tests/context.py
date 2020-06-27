e385b25b947ed00f27297161060631e6c0263cdd
Jimmy Charnley Kromann <jimmy@charnley.dk>
2020-06-27 23:25:24 +0200

    init config

--- tests/context.py
+++ tests/context.py
@@ -8,3 +8,13 @@ sys.path.insert(0, parent)
 
 import ppqm
 
+# TODO Set config for testing
+# def ini_settings(filename):
+#     config = configparser.ConfigParser()
+#     config.read(filename)
+#     return config
+#
+# # Init enviroment
+# CONFIG = ini_settings("development.ini")
+# Path(SCR).mkdir(parents=True, exist_ok=True)
+
