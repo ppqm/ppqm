b3010d45bb6f3d7000fc91b8d1ca459739ebe717
Jimmy Charnley Kromann <jimmy.kromann_ext@novartis.com>
2021-09-21 21:47:20 +0200

    setup init

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import setuptools  # type:ignore
+
+setuptools.setup(
+    name="ppqm",
+    version="0",
+    maintainer="Jimmy Kromann",
+    python_requires=">=3.6",
+    install_requires=[],
+    packages=["ppqm"],
+)
